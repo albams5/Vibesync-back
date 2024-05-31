fd925902598d29f718c597def2860fd5252d4c89
aoteron <aoteronunez@gmail.com>
2024-05-31 09:39:57 +0200

    fix(server): changes localhost for deployed front

--- src/server.ts
+++ src/server.ts
@@ -35,7 +35,7 @@ app.use(cookieParser())
 
 //conexion con el front
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://vibesyncfront-nj6givumb-aoterons-projects.vercel.app/",
     credentials: true
 }))
 
